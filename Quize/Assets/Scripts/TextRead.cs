0a41f15150afecb09ecaa1f5ea53b4482950eb43
borbos2001 <borbos-nik@mail.ru>
2022-11-06 19:35:49 +0300

    FirstCammit

---
+++ Quize/Assets/Scripts/TextRead.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TextRead : MonoBehaviour
+{
+    private string _fullText;
+    private void Start()
+    {
+        _fullText = Resources.Load("text1").ToString();
+        ChangeToRead();
+    }
+
+
+    private void Update()
+    {
+        
+    }
+    private void ChangeToRead()
+    {
+        char[] _charToCorrect = { ' ', '*', '.', ',', '!', '?'};
+        _fullText = _fullText.ToUpper();
+        string[] q = _fullText.Split(_charToCorrect, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string i in q)
+        {
+            Debug.Log(q);
+        }
+    }
+}
